235b32265bebe5d34db5152d0e68a797974bcdf8
rasheedbilly <rasheedbilly@gmail.com>
2021-12-08 19:53:57 -0500

    about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,6 +11,16 @@ function App() {
     return (
         <div class="jumbotron text-center">
             <h1>About Page</h1>
+            <br/>
+            <p>This app is to convert your playlist from a youtube playlist URL to a Spotify Playlist</p>
+            <ol>
+                <li>Get Youtube playlist URL and copy to clipboard</li>
+                <li>At Home page, login to Spotigy account by clicking on <b>Login in to Spotify</b></li>
+                <li>Hit <b>Accept in Spotify window</b></li>
+                <li>Paste Youtube URL into text box</li>
+                <li>Hit <b>Convert</b></li>
+            </ol>
+            <br/>
     
             {checkout ? (
                 <Paypal />
